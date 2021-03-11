85bfa7716feeb38476a7b58375b1e6b37e6bedac
Neal Davis <davis68@illinois.edu>
2021-03-11 11:44:48 -0600

    Post reviews and holden.

--- holden.py
+++ holden.py
@@ -21,9 +21,9 @@ class holden:
     def get_id_from_item(self,item):
         return self.df[self.df['Item_Description'] == item]['Item_id'].iloc[0]
 
-    def get_index_from_id(id):
+    def get_index_from_id(self,id):
         return self.df[self.df['Item_id'] == id].index[0]
-    def get_item_from_index(index):
+    def get_item_from_index(self,index):
         return self.df[self.df.index == index]['Item_Description'].iloc[0]
 
     def initialize(self,df):
@@ -40,20 +40,25 @@ class holden:
         self.df['Combined'] = self.df.apply(combine,axis=1)
 
         cv = CountVectorizer()
-        count_matrix = cv.fit_transform(selfdf['Combined'])
+        count_matrix = cv.fit_transform(self.df['Combined'])
         
         self.cosine_sim = cosine_similarity(count_matrix)
     
     def match(self,item):
-        item_index = self,get_index_from_id(self.get_id_from_item(item))
-        similar_soft_drinks = list(enumerate(cosine_sim[item_index]))
+        item_index = self.get_index_from_id(self.get_id_from_item(item))
+        similar_soft_drinks = list(enumerate(self.cosine_sim[item_index]))
         similar_sorted = sorted(similar_soft_drinks,key=lambda x:x[1],reverse=True)[1:]
 
         best_matches = []
         for i in range(0,5):
             try:
-                best_matches.append(self.get_item_from_index(similar_sorted[i][0]))
+                match = self.get_item_from_index(similar_sorted[i][0])
+                if march[1] > 0.0:
+                    best_matches.append(match)
+                else:
+                    break
             except IndexError:
-                print('Empty DF')
-                continue
+                print('Empty DF---has the item been reviewed?')
+                return []
+        return best_matches
 
